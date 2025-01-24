1e1db489082d3c7210571c53603e0ac73541a3c0
SuitBarlord <160561552+SuitBarlord@users.noreply.github.com>
2025-01-24 23:00:08 +0900

    whitelist

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telebot.async_telebot import AsyncTeleBot
 from dotenv import load_dotenv
 from engine.engine import Engine
 
-
+WHITE_LIST = []
 load_dotenv()
 
 bot = AsyncTeleBot(os.getenv("TELEGRAM_TOKEN"))
@@ -19,7 +19,13 @@ async def welcome(message):
 
 @bot.message_handler(content_types=['text'])
 async def handle_imei_check(message):
+    # Проверяем, есть ли пользователь в белом списке
+    if message.from_user.id not in WHITE_LIST:
+        bot.send_message(message.chat.id, "У вас нет доступа к этой команде.")
+        return
+
     await engine.handle_imei_check(message)
+    
 
 
 
